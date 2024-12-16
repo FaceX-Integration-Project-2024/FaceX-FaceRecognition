c253bd875a83d3faa9e49976d5c31c2f658849b4
Alocrl <146443967+Alocrl@users.noreply.github.com>
2024-12-16 12:26:56 +0100

    .

--- utilitaire/lcd.py
+++ utilitaire/lcd.py
@@ -82,20 +82,20 @@ def lcd_write(message):
         lcd_send(ord(char), False)
 
 # Programme principal
-try:
-    lcd_init()
-    print("LCD initialisé.")
+# try:
+lcd_init()
+print("LCD initialisé.")
 
-    # Exemple d'affichage
-    lcd_set_cursor(0, 0)  # Ligne 1, colonne 0
-    lcd_write("Hello FaceX!")
-    print("Message affiché : Hello FaceX!")
+# Exemple d'affichage
+lcd_set_cursor(0, 0)  # Ligne 1, colonne 0
+lcd_write("Hello FaceX!")
+print("Message affiché : Hello FaceX!")
 
-    lcd_set_cursor(1, 0)  # Ligne 2, colonne 0
-    lcd_write("Bienvenue")
-    print("Message affiché : Bienvenue")
+lcd_set_cursor(1, 0)  # Ligne 2, colonne 0
+lcd_write("Bienvenue")
+print("Message affiché : Bienvenue")
     
-finally:
-    # Nettoyer les broches GPIO en quittant
-    GPIO.cleanup()
-    print("GPIO nettoyées.")
+# finally:
+#     # Nettoyer les broches GPIO en quittant
+#     GPIO.cleanup()
+#     print("GPIO nettoyées.")

e479b1ce73776c9ce283895b168b5eb0a68b5e57
Ichiky_Otsu <bricedelcroix20@gmail.com>
2024-12-17 15:29:04 +0100

    Correction des TEST

--- database/face_data.py
+++ database/face_data.py
@@ -1,6 +1,5 @@
 from utilitaire.face_recognition_utils import studentsImgToFaceData
 
-
 def update_face_data(supabase, email):
     face_data = [studentsImgToFaceData(supabase, email)]
     
@@ -10,9 +9,12 @@ def update_face_data(supabase, email):
             "new_face_data": face_data
         }).execute()
 
-        if response.error:
-            raise TimeoutError(f"Erreur lors de la mise à jour des données faciales : {response.error}")
-        
+        # Access the error and status correctly
+        if response.get("error"):  # Use .get() to safely check for "error" key
+            raise Exception(f"Erreur lors de la mise à jour des données faciales : {response['error']}")
+
         return face_data
-    except TimeoutError  as e:
+
+    except Exception as e:
         print(f"Erreur lors de l'exécution de la procédure stockée pour {email}: {e}")
+        return None

808b302cacc20c8f3381549051f82691665d026d
Alocrl <146443967+Alocrl@users.noreply.github.com>
2024-12-18 11:37:32 +0100

    bugfix identified person

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def main():
 
 
     existing_attendance = getAttendanceForBlock(supabase, block_id)
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     print("Webcam démarrée")
 
     if not cap.isOpened():
